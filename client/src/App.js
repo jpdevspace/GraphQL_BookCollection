c98fc5a90f510f379581dd2917ad2d53633cb244
Jean Paul <jjpwebdev@gmail.com>
2019-06-01 16:58:15 -0400

    added Apollo client to the react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
-const App = () => {
-  return (
+// Components
+import BookList from './components/BookList'
+
+// Apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
+
+const App = () => (
+  <ApolloClient client={client}>
     <div id="main">
       <h1>Ninja's Reading List</h1>
+      <BookList />
     </div>
-  );
-}
+  </ApolloClient>
+);
+
 
 export default App;
